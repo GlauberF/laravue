6ccc2c52e104dd3f210ac52aec594fb79321f8e9
Joca <jpereyraleon@gmail.com>
2016-10-05 15:43:44 -0500

    clean composer lock, add phpdoc to example test

--- tests/php/ExampleTest.php
+++ tests/php/ExampleTest.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    /**
+    * A basic functional test example.
+    *
+    * @return void
+    */
     public function testBasicExample()
     {
         $this->visit('/')
